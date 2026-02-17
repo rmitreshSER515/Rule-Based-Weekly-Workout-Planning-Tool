e908258f7c3d68442e84fef2e3e9782c95441058
spate372 <spate372@asu.edu>
2026-02-17 13:32:51 -0700

    Created User Registration Page (#7)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
 import FitnessTrackerPage from "./components/FitnessTrackerPage";
+import RegisterPage from "./components/RegisterPage";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
       <Route path="/fitness" element={<FitnessTrackerPage />} />
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
