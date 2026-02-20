7b699dac7e26f520965942c17aa41e9cfeb68551
Harshil Dave <hdave12@asu.edu>
2026-02-20 01:12:51 -0700

    remove TypeORM entity and use Mongoose schema

--- backend/src/rules/rules.module.ts
+++ backend/src/rules/rules.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { RulesController } from './rules.controller';
 import { RulesService } from './rules.service';
+import { Rule, RuleSchema } from './entities/rule.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Rule.name, schema: RuleSchema }]),
+  ],
   controllers: [RulesController],
-  providers: [RulesService]
+  providers: [RulesService],
+  exports: [RulesService],
 })
 export class RulesModule {}
