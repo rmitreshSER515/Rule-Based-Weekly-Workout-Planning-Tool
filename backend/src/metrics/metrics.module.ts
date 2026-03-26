f838b106cd1f407863d3c2aaf403d3a8f38cfec4
rmitreshSER515 <rmitresh@asu.edu>
2026-03-26 16:25:30 -0700

    Code changes to compare the number of exercises under each intensity.

--- backend/src/metrics/metrics.module.ts
+++ backend/src/metrics/metrics.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MetricsController } from './metrics.controller';
 import { MetricsService } from './metrics.service';
+import { SchedulesModule } from '../schedules/schedules.module';
 
 @Module({
+  imports: [SchedulesModule],
   controllers: [MetricsController],
-  providers: [MetricsService]
+  providers: [MetricsService],
 })
 export class MetricsModule {}
